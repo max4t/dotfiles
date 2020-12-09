23255144a32a6b427eec54640d0434abb91cf890
Maxime Toumi <maxime.tm@gmail.com>
2020-12-09 02:12:12 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+xcode_cli_installed() {
+    xcode-select -p 2> /dev/null
+    if [ "$(echo $?)" -ne "0" ]; then
+        echo "Run 'xcode-select --install'" >&2
+        exit 1
+    fi
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xcode_cli_installed
+else
+    echo "Only MacOS is supported" >&2
+    exit 1
+fi
