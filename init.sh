37c52023d66897ff593840e63017ff157803927f
Maxime Toumi <maxime.tm@gmail.com>
2020-12-09 03:01:09 +0100

    Init install (#1)
    * Create install.sh
    
    * Create .chezmoiignore
    
    * Create init.sh
    
    * Update .chezmoiignore
    
    * Update init.sh
    
    * Update init.sh
    
    * Update README.md

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+xcode_cli_installed() {
+    xcode-select -p 2>&1 > /dev/null
+    if [ "$(echo $?)" -ne "0" ]; then
+        echo "Run 'xcode-select --install'" >&2
+        exit 1
+    fi
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xcode_cli_installed
+else
+    echo "Only MacOS is supported" >&2
+    exit 1
+fi
+
+if [ -d "dotfiles" ]; then
+    echo "'dotfiles' already exists. Remove it or run 'install.sh'" >&2
+    exit 1
+fi
+
+git clone git@github.com:max4t/dotfiles.git
+cd dotfiles
+
+./install.sh
